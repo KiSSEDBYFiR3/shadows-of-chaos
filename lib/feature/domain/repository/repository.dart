5e6d5ca4c26a715e45a9854af425107057737a24
Stanislav Moiseev <72256017+KiSSEDBYFiR3@users.noreply.github.com>
2022-08-16 22:50:45 +0300

    Delete repository.dart

--- lib/feature/domain/repository/repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:shadows_beta_01/core/error/failure/failure.dart';
-import 'package:shadows_beta_01/feature/domain/entities/base_entity.dart';
-
-abstract class NovelRepository {
-  Future<Either<Failure, BaseEntity>> getNovelData();
-  Future<Either<Failure, BaseEntity>> updateNovelData();
-}
